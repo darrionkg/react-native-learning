e048dbee12ea579646aff5e39abe5d61ad151eb1
Darrion Gering <darrionkg@gmail.com>
2021-10-06 18:07:04 -0700

    add navigation buttons to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>hi</Text>
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>hi</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title='Go To Components Demo'
+      />
+      <Button
+        title='Go To List Demo'
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
